431275bdb6243f514f2b1ae861626c41f080f713
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-08-23 00:35:27 +0530

    Add Framer-Motion

---
+++ app/components/ClientLayout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import NavBar from "./Navbar/NavBar";
+import Footer from "./Footer/Footer";
+import { ScrollProvider } from "@/context/ScrollContext";
+import { usePathname } from "next/navigation";
+import { slide, opacity, perspective } from "../anim";
+
+
+export default function ClientLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    const pathname = usePathname(); // Get the current path
+  
+    return (
+      <ScrollProvider>
+        <NavBar />
+        <AnimatePresence mode="wait">
+          <motion.div className="inner" key={pathname}>
+            <motion.div className="slide" initial="initial" animate="enter" exit="exit" variants={slide} />
+            <motion.div className="page" initial="initial" animate="enter" exit="exit" variants={perspective}>
+              <motion.div initial="initial" animate="enter" exit="exit" variants={opacity}>
+                {children}
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </AnimatePresence>
+        <Footer />
+      </ScrollProvider>
+    );
+  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,23 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/Navbar/NavBar";
-import Footer from "./components/Footer/Footer";
-import { ScrollProvider } from "@/context/ScrollContext";
-
+import ClientLayout from "./components/ClientLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Ashish Mohapatra",
   description: "Portfolio of Ashish Mohapatra",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ScrollProvider>
-          <NavBar />
-          {children}
-          <Footer />
-        </ScrollProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
