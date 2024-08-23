22b12adea2321dd1665480aab887164eda1777e1
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-08-23 19:53:54 +0530

    Removed MediaQueries in useGSAP

--- app/useGsap.tsx
+++ app/useGsap.tsx
@@ -10,56 +10,25 @@ if (typeof window !== "undefined") {
 
 const useGsap = (sections: React.RefObject<HTMLDivElement[]>) => {
   useEffect(() => {
-    const mm = gsap.matchMedia();
-
-    mm.add("(min-width: 600px)", () => {
-      const triggers = sections.current?.map((section) => {
-        if (section) {
-          return ScrollTrigger.create({
-            trigger: section,
-            start: "top top",
-            end: () => `+=${section.offsetHeight}`,
-            pin: true,
-            pinSpacing: false,
-            scrub: 5,
-            onEnter: () => gsap.to(section, { y: 16, duration: 0.2 }),
-            onLeaveBack: () => gsap.to(section, { y: 0, duration: 0.5 }),
-            onLeave: () => gsap.to(section, { y: 0, duration: 0.5 }),
-          });
-        }
-        return null;
-      });
-
-      return () => {
-        triggers?.forEach((trigger) => trigger?.kill());
-      };
-    });
-
-    mm.add("(max-width: 599px)", () => {
-      const triggers = sections.current?.map((section) => {
-        if (section) {
-          return ScrollTrigger.create({
-            trigger: section,
-            start: "top top",
-            end: () => `+=${section.offsetHeight}`,
-            pin: true,
-            pinSpacing: true,
-            scrub: 2,
-            onEnter: () => gsap.to(section, { y: 8, duration: 0.2 }),
-            onLeaveBack: () => gsap.to(section, { y: 0, duration: 0.5 }),
-            onLeave: () => gsap.to(section, { y: 0, duration: 0.5 }),
-          });
-        }
-        return null;
-      });
-
-      return () => {
-        triggers?.forEach((trigger) => trigger?.kill());
-      };
+    const triggers = sections.current?.map((section) => {
+      if (section) {
+        return ScrollTrigger.create({
+          trigger: section,
+          start: "top top",
+          end: () => `+=${section.offsetHeight}`,
+          pin: true,
+          pinSpacing: false,
+          scrub: 1,
+          onEnter: () => gsap.to(section, { y: 16, duration: 0.2 }),
+          onLeaveBack: () => gsap.to(section, { y: 0, duration: 0.5 }),
+          onLeave: () => gsap.to(section, { y: 0, duration: 0.5 }),
+        });
+      }
+      return null;
     });
 
     return () => {
-      mm.revert();
+      triggers?.forEach((trigger) => trigger?.kill());
     };
   }, [sections]);
 };
